44ae918ca4d4a7c8bfdc194356088185eda1431e
Jose Indalecio <riosindalecio@hotmail.com>
2023-05-22 15:35:04 -0600

    separar imagenes en carpetas diferentes

--- cleandata/separateimage.py
+++ cleandata/separateimage.py
@@ -2,26 +2,23 @@ import csv
 import os
 import shutil
 
-def move_images_from_csv(csv_file, destination_folder):
-    try:
-        with open(csv_file, 'r') as file:
+def move_images_from_csv(csv_file, destination_folder, origin_images):
+    with open(csv_file, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 image_id = row['image_id']
                 dx = row['dx']
-                source_image = image_id + '.jpg'
+                source_image =origin_images + image_id + '.jpg'
                 dx_folder = os.path.join(destination_folder, dx)
-
                 if not os.path.exists(dx_folder):
                     os.makedirs(dx_folder)
-
+                
                 shutil.move(source_image, dx_folder)
                 print(f"Image {source_image} moved to folder {dx_folder}")
-    except FileNotFoundError:
-        print("The CSV file does not exist.")
 
 # Usage example
-csv_file = "path/to/csv/file.csv"
-destination_folder = "path/to/destination/folder"
+csv_file = "D:/dataset/HAM10000_metadata.csv"
+destination_folder = "D:/dataset/image_separate/"
+origin_images = "D:/dataset/HAM10000_images_part_1/"
 
-move_images_from_csv(csv_file, destination_folder)
+move_images_from_csv(csv_file, destination_folder, origin_images)
