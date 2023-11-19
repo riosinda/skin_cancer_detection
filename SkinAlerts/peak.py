9bde5260c8a8fc565715c7d09369d5323b3f6544
jriosrios <riosindalecio@hotmail.com>
2023-11-19 00:51:47 -0600

    predictions

--- SkinAlerts/peak.py
+++ SkinAlerts/peak.py
@@ -1,97 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import keras
+import cv2 as cv
+import numpy as np
+from werkzeug.utils import secure_filename
+import os
+
+
+model = keras.models.load_model('cnn2_MobileNet_700_black.h5')
+print('Model loaded')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'D:/SkinAlertsDataset'
 
 @app.route('/')
 def index():
-    pages = [
-        '/',
-        '/about',
-        '/information',
-        '/detection',
-        '/database',
-        '/dataprivacy',
-        '/contact']
-    
-    return render_template('index.html', pages=pages)
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    pages = [
-        '/',
-        '/about',
-        '/information',
-        '/detection',
-        '/database',
-        '/dataprivacy',
-        '/contact']
-    
-    return render_template('about.html', pages=pages)
+    return render_template('about.html')
 
 @app.route('/information')
 def information():
-    pages = [
-        '/',
-        '/about',
-        '/information',
-        '/detection',
-        '/database',
-        '/dataprivacy',
-        '/contact']
-    
-    return render_template('information.html', pages=pages)
+    return render_template('information.html')
 
-@app.route('/detection')
-def detection():
-    pages = [
-        '/',
-        '/about',
-        '/information',
-        '/detection',
-        '/database',
-        '/dataprivacy',
-        '/contact']
-    
-    return render_template('detection.html', pages=pages)
+
+@app.route('/detection', methods=['GET', 'POST'])
+def detection(model=model):
+    if request.method == "POST":
+        image = request.files['file']
+
+        if image.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        
+        filename = secure_filename(image.filename)
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        img = cv.imread('D:/SkinAlertsDataset/' + filename)
+        rimg1 = cv.resize(img, dsize=(150, 150), interpolation=cv.INTER_CUBIC)
+        image = rimg1.astype('float32') / 255
+
+        if round(model.predict(np.array([image]))[0][0]) == 1:
+            print('bcc')
+        else:
+            print('scc')
+
+        return render_template('detection.html')
+        
+    return render_template('detection.html')
+
+
 
 @app.route('/database')
 def database():
-    pages = [
-        '/',
-        '/about',
-        '/information',
-        '/detection',
-        '/database',
-        '/dataprivacy',
-        '/contact']
-    
-    return render_template('database.html', pages=pages)
+    return render_template('database.html')
 
 @app.route('/dataprivacy')
 def dataprivacy():
-    pages = [
-        '/',
-        '/about',
-        '/information',
-        '/detection',
-        '/database',
-        '/dataprivacy',
-        '/contact']
-    
-    return render_template('dataprivacy.html', pages=pages)
+    return render_template('dataprivacy.html')
 
 @app.route('/contact')
 def contact():
-    pages = [
-        '/',
-        '/about',
-        '/information',
-        '/detection',
-        '/database',
-        '/dataprivacy',
-        '/contact']
-    
-    return render_template('contact.html', pages=pages)
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
